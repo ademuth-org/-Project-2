34a59e7c78b0c0a3cfdf5afbeb6f3b98cc8574cc
ademuth <89747240+ademuth@users.noreply.github.com>
2022-12-02 18:05:34 -0600

    added am and pm

--- js/script.js
+++ js/script.js
@@ -53,4 +53,13 @@ function showTime(){
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
     var session = "AM";
+	
+	if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
 }
