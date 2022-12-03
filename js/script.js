f8455b900a560ed12c1505926f4df430df817d5b
ademuth <89747240+ademuth@users.noreply.github.com>
2022-12-02 18:02:06 -0600

    creating js variables to make a clock
    making a clock for the java tool. This is technically obstructive because it runs without any sort of interaction from the user, but come on its a clock is it gonna kill them to know the time against their will

--- js/script.js
+++ js/script.js
@@ -45,3 +45,12 @@ function validateform(){
     alert("Please enter your message");  
   }
 }
+
+//JS Clock
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+}
