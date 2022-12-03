cfb871fc70f05c76e22dfc46da2a32c7a6a1ee8d
ademuth <89747240+ademuth@users.noreply.github.com>
2022-12-02 18:15:03 -0600

    added hour, minute, and second functionality

--- js/script.js
+++ js/script.js
@@ -53,13 +53,14 @@ function showTime(){
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
     var session = "AM";
-	
 	if(h == 0){
         h = 12;
     }
-    
     if(h > 12){
         h = h - 12;
         session = "PM";
-    }
+    }	
+	h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
 }
